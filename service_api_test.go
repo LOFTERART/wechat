074f9c6b0c226033974b576b07a86abdc3dad61b
leexsoft-mac <leexsoft@163.com>
2019-09-05 16:54:53 +0800

    拆分文件

--- service_api_test.go
+++ service_api_test.go
@@ -2,7 +2,6 @@ package wechat
 
 import (
 	"fmt"
-	"os"
 	"testing"
 )
 
@@ -30,57 +29,4 @@ func TestBasicUserInfo(t *testing.T) {
 		return
 	}
 	t.Logf("返回值: %+v\n", user)
-}
-
-func TestAuthAccessToken(t *testing.T) {
-	fmt.Println("----------获取授权的access_token----------")
-	// 请求接口
-	appId := os.Getenv("AppID")
-	appSecret := os.Getenv("ApiSecret")
-	code := ""
-	token, err := GetAuthAccessToken(appId, appSecret, code)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	t.Logf("返回值: %+v\n", token)
-}
-
-func TestAuthUserInfo(t *testing.T) {
-	fmt.Println("----------获取用户基本信息(授权机制)----------")
-	// 请求接口
-	token := ""
-	openId := ""
-	user, err := GetAuthUserInfo(token, openId, "")
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	t.Logf("返回值: %+v\n", user)
-}
-
-func TestRefreshAuthAccessToken(t *testing.T) {
-	fmt.Println("----------刷新授权的access_token----------")
-	// 请求接口
-	appId := os.Getenv("AppID")
-	refresh := ""
-	token, err := RefreshAuthAccessToken(appId, refresh)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	t.Logf("返回值: %+v\n", token)
-}
-
-func TestCheckAuthAccessToken(t *testing.T) {
-	fmt.Println("----------校验授权的access_token----------")
-	// 请求接口
-	token := ""
-	openId := ""
-	rst, err := CheckAuthAccessToken(token, openId)
-	if err != nil {
-		t.Error(err)
-		return
-	}
-	t.Logf("返回值: %+v\n", rst)
 }
---
+++ service_auth_api.go
@@ -0,0 +1,57 @@
+/*
+ 网页授权相关的接口。
+*/
+
+package wechat
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// 获取网页授权的access_token
+func GetAuthAccessToken(appId, appSecret, code string) (accessToken AccessToken, err error) {
+	url := fmt.Sprintf("https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code", appId, appSecret, code)
+	body, err := httpGet(url)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &accessToken)
+	return
+}
+
+// 刷新网页授权的access_token
+func RefreshAuthAccessToken(appId, refreshToken string) (accessToken AccessToken, err error) {
+	url := fmt.Sprintf("https://api.weixin.qq.com/sns/oauth2/refresh_token?appid=%s&grant_type=refresh_token&refresh_token=%s", appId, refreshToken)
+	body, err := httpGet(url)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &accessToken)
+	return
+}
+
+// 获取用户基本信息(授权机制)
+func GetAuthUserInfo(accessToken, openId, lang string) (userInfo UserInfo, err error) {
+	if len(lang) <= 0 {
+		lang = "zh_CN"
+	}
+	url := fmt.Sprintf("https://api.weixin.qq.com/sns/userinfo?access_token=%s&openid=%s&lang=%s", accessToken, openId, lang)
+	body, err := httpGet(url)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &userInfo)
+	return
+}
+
+// 检验网页授权的access_token是否有效
+func CheckAuthAccessToken(accessToken, openId string) (resp ResponseBase, err error) {
+	url := fmt.Sprintf("https://api.weixin.qq.com/sns/auth?access_token=%s&openid=%s", accessToken, openId)
+	body, err := httpGet(url)
+	if err != nil {
+		return
+	}
+	err = json.Unmarshal(body, &resp)
+	return
+}
